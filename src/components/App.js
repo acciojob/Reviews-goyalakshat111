db9240f00b7d4002594abd8335bca53de49363cf
Akshat Goyal <goyalakshat111@gmail.com>
2025-04-05 05:26:30 +0530

    reviews

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = ()=>{
         <div>
             <main>
                 <section className="container">
-                    <h1 id='review-heading'> Our Reviews</h1>
+                    <h1 id='review-heading'>Our Reviews</h1>
                     <Review/>
                 </section>
             </main>
